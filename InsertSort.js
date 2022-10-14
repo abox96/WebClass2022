1ff1bc8282e5ce7bf7e92fb2a893281cbf5c1e85
Min-Jae Ahn <99654475+abox96@users.noreply.github.com>
2022-10-14 21:26:52 +0900

    어제의 정렬알고리즘구현에 대한 설명추가

--- InsertSort.js
+++ InsertSort.js
@@ -1,16 +1,12 @@
-
-let myarray = new Array(30);
+let myarray = new Array(150);
 myarray.splice(0, myarray.length);
 
-
-for(i=0; i <= 30; i++){
+for(i=0; i <= 150; i++){
    a = Math.floor(Math.random() * 100);
    myarray[i] = a;
 }
+
 function insertionSort (array) {
-
-    
-
       for (let i = 1; i < array.length; i++) {
             let cur = array[i];
             let left = i - 1;    
@@ -25,5 +21,12 @@ function insertionSort (array) {
         
         return array;
     }
-    
     console.log(insertionSort(myarray));
+
+/*삽입정렬은 항상 두 번째 요소를 왼쪽 요소와 비교하면서 시작한다.
+이렇게 두 요소를 비교하는 것을 배열의 끝까지 반복하여 1회차 탐색이 끝나며,
+모든 요소가 정렬될 때까지 회차를 반복하게 된다.
+그렇기 때문에 Big O는 정렬이 하나도 안되어 있는 최악일 경우 O(n^2)
+이미 정렬되어 있는 최고의 경우 O(n)의 시간 복잡도를 가지게 된다.
+삽입정렬의 장점 중 하나는 정렬과정 중 또다른 메모리를 할당할 필요없이 기존의 메모리안에서 정렬이 가능하기에 메모리가 절약된다는 점이다.
+반면에 자료의 개수가 많아질수록 탐색해야 하는 데이터가 많기에 성능이 매우 떨어지게 된다.*/
--- QuickSort.js
+++ QuickSort.js
@@ -18,13 +18,16 @@ const quickSort = function (arr) {
    
   };
 
-  let myarray = new Array(30);
+let myarray = new Array(150);
 myarray.splice(0, myarray.length);
 
-
-for(i=0; i <= 30; i++){
+for(i=0; i <= 150; i++){
    a = Math.floor(Math.random() * 100);
    myarray[i] = a;
 }
 console.log(myarray);
 console.log(quickSort(myarray));
+/*퀵 정렬은 기준이 되는 요소 하나를 정하고 왼쪽에 기준요소보다 작은 값의 배열을 큰 값은 오른쪽에 놓는다.
+이렇게 분할해 놓은 하위 배열에 대해 재귀적으로 퀵 정렬을 호출한다.
+이러한 퀵 정렬은 별도의 메모리 공간을 할당해줘야 하기에 데이터 양이 많으면 공간 낭비가 심해진다.
+퀵 정렬의 시간 복잡도는 최악의 경우  O(n^2)와 최상의 경우 O(nlog₂n)를 가진다.*/
